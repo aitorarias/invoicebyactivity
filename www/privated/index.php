c7907d5b34cdce02956c567429e2f006b3f45d0e
aitorarias <aitor.acruz@gmail.com>
2019-03-19 19:50:00 +0100

    UI issues

--- www/privated/index.php
+++ www/privated/index.php
@@ -11,26 +11,98 @@
     <script>
         alert("Esta página es de uso exclusivo para los administradores. Por favor, si no tienes dicho rol, abandónala. Tu dirección IP será guardada por motivos de seguridad. Gracias. ")
     </script>
-    <div id="header">
-        Bienvenid@ a la administración
+    <nav class="navbar is-danger">
+    <div class="navbar-brand">
+    <a class="navbar-item">
+        <i class="fas fa-user-secret"></i>
+    </a>
+    <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
-    <?php
-    if(isset($_SESSION['valid'])) {            
-        include("connection.php");                    
-        $result = mysqli_query($mysqli, "SELECT * FROM login");
-    ?>                
-        Bienvenido <?php echo $_SESSION['nombre'] ?> ! <a href='logout.php'>Logout</a><br/>
-        <br/>
-        <a href='view.php'>Ver la lista de usuarios dados de alta en la plataforma</a>
-        <br/><br/>
-    <?php    
+  </div>
+
+  <div id="navbarExampleTransparentExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="../index.html">
+        Página principal
+      </a>
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link" href="https://bulma.io/documentation/overview/start/">
+          Documentación
+        </a>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="field is-grouped">
+          <p class="control">
+          <?php
+            if(isset($_SESSION['valid'])){            
+                include("connect.php");                    
+                $result = mysqli_query($mysqli, "SELECT * FROM login");
+            ?>            
+            <a class="button is-primary" href="./logout.php">
+              <span class="icon">
+                <i class="fas fa-sign-out-alt"></i>
+              </span>
+              <span>Logout</span>
+            </a>
+            <?php    
     } else {
-        echo "Debes estar logueado para ver esta página.<br/><br/>";
-        echo "<a href='login.php'>Login</a> | <a href='register.php'>¡Regístrate como personal autorizado!</a>";
+        echo "<a class='button is-primary' href='login.php'>Login</a>  <a class='button is-link' href='register.php'>¡Regístrate como personal autorizado!</a>";
     }
     ?>
-    <div id="footer">
-        Aitor Arias Cruz | 2019 Examen Final Xestión Empresarial
+          </p>
+        </div>
+      </div>
     </div>
+  </div>
+</nav>
+
+<section class="hero is-info is-fullheight">
+  <!-- Hero content: will be in the middle -->
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <h1 class="title">
+        Bienvenido
+      </h1>
+      <h2 class="subtitle">
+      <?php
+    if(isset($_SESSION['valid'])) {            
+        include("connect.php");                    
+        $result = mysqli_query($mysqli, "SELECT * FROM login");
+    ?>                
+        Acceso permitido <?php echo $_SESSION['nombre'] ?>!
+        <br/> <br/><br/>
+        <a href='view.php'>Puedes ver la lista de usuarios dados de alta en la plataforma</a>
+    <?php    
+    } else {
+        echo "<br/><br/>Acceso restringido. Debes estar logueado para ver esta página.<br/><br/>";
+    }
+    ?> 
+      </h2>
+    </div>
+  </div>
+      <div class="hero-foot">
+    <nav class="tabs is-boxed is-fullwidth">
+      <div class="container">
+        <ul>
+          <li>  
+            <a href="https://github.com/aitorbuleapp/">GitHub</a>
+          </li>
+          <li>
+            <a href="http://www.aarias.colexio-karbo.com/18-19/">Colexio Karbo</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/aitorariascruz/?originalSubdomain=es">Aitor Arias</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</section>
 </body>
 </html>
--- www/privated/register.php
+++ www/privated/register.php
@@ -1,10 +1,40 @@
 <html>
 <head>
-    <title>Register</title>
+    <title>Registro</title>
+    <title>Acceso restringido</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
  
 <body>
-    <a href="index.php">Home</a> <br />
+<nav class="navbar is-danger">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="./index.php">
+        <i class="fas fa-user-secret"></i>
+        </a>
+    <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+
+  <div id="navbarExampleTransparentExample" class="navbar-menu">
+    <div class="navbar-start">
+    <a class="navbar-item">
+        Regístrate introduciendo todos los campos
+      </a>
+      <a class="navbar-item" href="./index.php">
+        Vuelve atrás 🔙
+      </a>
+    </div>
+
+    </div>
+  </div>
+</nav>
+
     <?php
     include("connect.php");
  
@@ -15,37 +45,35 @@
         $pass = $_POST['password'];
  
         if($user == "" || $pass == "" || $nombre == "" || $email == "") {
-            echo "All fields should be filled. Either one or many fields are empty.";
-            echo "<br/>";
-            echo "<a href='register.php'>Go back</a>";
+            echo "Todos los campos han de ser cubiertos.";
+            echo "<a href='register.php'>Vuelve atrás</a>";
         } else {
             mysqli_query($mysqli, "INSERT INTO login(nombre, email, username, password) VALUES('$nombre', '$email', '$user', md5('$pass'))")
-            or die("Could not execute the insert query.");
+            or die("No se ha podido establecer conexión con la base de datos.");
             
-            echo "Registration successfully";
-            echo "<br/>";
-            echo "<a href='login.php'>Login</a>";
+            echo "Registro completado con éxito";
+            echo "<a href='login.php'>Ahora, logueate</a>";
         }
     } else {
 ?>
-        <p><font size="+2">Register</font></p>
+            <br/><br/>
         <form name="form1" method="post" action="">
-            <table width="75%" border="0">
+            <table class="table">
                 <tr> 
-                    <td width="10%">Full Name</td>
-                    <td><input type="text" name="nombre"></td>
+                    <td>Nombre</td>
+                    <td><input type="text" name="nombre" class="input is-primary" placeholder="Introduzca su nombre"></td>
                 </tr>
                 <tr> 
                     <td>Email</td>
-                    <td><input type="text" name="email"></td>
+                    <td><input type="text" name="email" class="input is-primary" placeholder="Introduzca su email"></td>
                 </tr>            
                 <tr> 
-                    <td>Username</td>
-                    <td><input type="text" name="username"></td>
+                    <td>Nombre de usuario</td>
+                    <td><input type="text" name="username" class="input is-primary" placeholder="Introduzca su nombre de usuario"></td>
                 </tr>
                 <tr> 
-                    <td>Password</td>
-                    <td><input type="password" name="password"></td>
+                    <td>Contraseña</td>
+                    <td><input type="password" name="password" class="input is-danger" placeholder="Introduzca su contraseña"></td>
                 </tr>
                 <tr> 
                     <td>&nbsp;</td>
